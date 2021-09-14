193df8665e86ca01082d645fe3d917254c34714e
Xiangguang <Xiangguang@qq.com>
2021-09-14 16:12:18 +0800

    hot-fix commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
 
     public static void main(String[] args) {
         System.out.println("hello");
-        System.out.println("hello4");
+        System.out.println("hello!!!");
+        System.out.println("hot-fix hello!!!");
     }
 }
